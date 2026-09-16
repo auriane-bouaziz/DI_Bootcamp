d614da184475873ce27a8370fbadc6e31ff49d34
Auriane <auriane.b8@gmail.com>
2026-09-16 17:57:12 +0300

    w6d4

---
+++ Week06/Day4/Ex_XP/xp.py
@@ -0,0 +1,13 @@
+#%%
+# 🌟 Exercise 1 : Matrix Operations
+# Instructions
+# In this exercise, you’ll work with a 3x3 matrix. Here’s a brief explanation of the concepts:
+
+# Determinant: The determinant is a value that can be computed from the elements of a square matrix. It provides important information about the matrix, such as whether it has an inverse, and is used in various areas like linear algebra and calculus. To understand more about it you can watch this video.
+# Inverse of a Matrix: The inverse of a matrix is a matrix that, when multiplied with the original matrix, results in an identity matrix. Not all matrices have inverses. The inverse is crucial in solving systems of linear equations.
+# Create a 3x3 matrix and perform the following operations:
+
+# Calculate the determinant.
+# Find the inverse of the matrix.
+
+
